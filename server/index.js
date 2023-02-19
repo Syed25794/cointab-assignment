69a9f6f1f975e54112aac0a111e934658833eebc
Syed25794 <syedgulmohammad25794@gmail.com>
2023-02-19 15:39:10 +0530

    done

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const connection = require("../config/database");
-const userRoutes = require("../routes/user.routes");
+const connection = require("./config/database");
+const userRoutes = require("./routes/user.routes");
 const cors = require("cors");
 
 const app = express();
