a60152961ca8295d47752e55c436814042bcdd4e
Syed25794 <syedgulmohammad25794@gmail.com>
2023-02-19 21:06:51 +0530

    added the filter option

--- client/src/pages/UserPage.jsx
+++ client/src/pages/UserPage.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import {
   Box,
   Button,
+  ButtonGroup,
+  Select,
   Table,
   TableContainer,
   Tbody,
@@ -10,6 +12,7 @@ import {
   Tr,
 } from "@chakra-ui/react";
 import TableRow from "../components/TableRow";
+import countries from './../data/country.json';
 
 const UserPage = () => {
   const [page, setPage] = useState(1);
@@ -27,13 +30,40 @@ const UserPage = () => {
       console.log(error);
     }
   };
+
+  const handleSelect=(e)=>{
+    const { name, value}=e.target;
+    console.log(name,value);
+  }
   useEffect(() => {
     getData();
   }, [page]);
   return (
     <Box>
-      <Button onClick={() => setPage((prev) => prev + 1)} isDisabled={ page === 10 }>Next</Button>
-      <Button onClick={() => setPage((prev) => prev - 1)} isDisabled={ page === 1 }>Previous</Button>
+      <Box display="flex" flexDir="row" w="900px" m="auto" onChange={handleSelect}>
+        <Select w="200px" m="20px" name="gender">
+          <option value="">Choose the gender</option>
+          <option value="men">Men</option>
+          <option value="women">Women</option>
+        </Select>
+        <Select w="200px" m="20px" name="age" onChange={handleSelect}>
+          <option value="">Choose the age</option>
+          <option value={25}>0-25</option>
+          <option value={50}>26-50</option>
+          <option value={75}>51-75</option>
+          <option value={76}>Above 75</option>
+        </Select>
+        <Select w="200px" m="20px" name="country" onChange={handleSelect}>
+          <option value="">Choose the country</option>
+          {countries?.map((country)=>(
+            <option key={country.name} value={country.name}>{country.name}</option>
+          ))}
+        </Select>
+      </Box>
+      <ButtonGroup float="right" marginRight="150px" marginBottom="15px">
+        <Button onClick={() => setPage((prev) => prev + 1)} colorScheme="blue" isDisabled={page === 10}>Next</Button>
+        <Button onClick={() => setPage((prev) => prev - 1)} colorScheme="blue" isDisabled={page === 1}>Previous</Button>
+      </ButtonGroup>
       <TableContainer
         border="1px solid black"
         w="80%"
@@ -42,7 +72,7 @@ const UserPage = () => {
         borderRadius="10px"
         marginBottom="20px"
       >
-        <Table variant="striped" >
+        <Table variant="striped">
           <Thead fontSize="30px">
             <Tr>
               <Th>Image</Th>
@@ -54,8 +84,8 @@ const UserPage = () => {
             </Tr>
           </Thead>
           <Tbody>
-            {data?.map((user) =>{
-              return <TableRow key={user._id} user={user} />
+            {data?.map((user) => {
+              return <TableRow key={user._id} user={user} />;
             })}
           </Tbody>
         </Table>
